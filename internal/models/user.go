d8fbcb1000a7e6fe044d5449a3d88b42ccf29274
bangiam <139111446+bangiam@users.noreply.github.com>
2023-07-12 09:42:03 +0700

    check
    test

--- internal/models/user.go
+++ internal/models/user.go
@@ -10,17 +10,17 @@ import (
 // User model
 type User struct {
 	Id        int       `gorm:"primarykey;column:id" json:"id" redis:"id"`
-	UserId    uuid.UUID `gorm:"column:user_id" json:"user_id" redis:"user_id" validate:"omitempty"`
-	FirstName string    `gorm:"column:first_name" json:"first_name" redis:"first_name" validate:"required,lte=30"`
-	LastName  string    `gorm:"column:last_name" json:"last_name" redis:"last_name" validate:"required,lte=30"`
-	Email     string    `gorm:"column:email" json:"email,omitempty" redis:"email" validate:"omitempty,lte=60,email"`
-	Password  string    `gorm:"column:password" json:"password,omitempty" redis:"password" validate:"omitempty,required,gte=6"`
-	School    string    `gorm:"column:school" json:"school" redis:"school" validate:"required,lte=30"`
-	Gender    string    `gorm:"column:gender" json:"gender,omitempty" redis:"gender" validate:"omitempty,lte=10"`
-	Birthday  time.Time `gorm:"column:birthday" json:"birthday,omitempty" redis:"birthday" validate:"required,lte=10"`
-	CreatedAt time.Time `gorm:"column:created_at" json:"created_at,omitempty" redis:"created_at"`
-	UpdatedAt time.Time `gorm:"column:updated_at" json:"updated_at,omitempty" redis:"updated_at"`
-	LoginDate time.Time `gorm:"column:login_date" json:"login_date,omitempty" redis:"login_date" validate:"omitempty"`
+	UserId    uuid.UUID `gorm:"column:user_id" json:"user_id" redis:"user_id"`
+	FirstName string    `gorm:"column:first_name" json:"first_name" redis:"first_name"`
+	LastName  string    `gorm:"column:last_name" json:"last_name" redis:"last_name"`
+	Email     string    `gorm:"column:email" json:"email,omitempty" redis:"email"`
+	Password  string    `gorm:"column:password" json:"password,omitempty" redis:"password"`
+	School    string    `gorm:"column:school" json:"school" redis:"school"`
+	Gender    string    `gorm:"column:gender" json:"gender,omitempty" redis:"gender"`
+	Birthday  time.Time `gorm:"column:birthday" json:"birthday,omitempty" redis:"birthday"`
+	CreatedAt time.Time `gorm:"autoCreateTime;column:created_at" json:"created_at,omitempty" redis:"created_at"`
+	UpdatedAt time.Time `gorm:"autoUpdateTime;column:updated_at" json:"updated_at,omitempty" redis:"updated_at"`
+	LoginDate time.Time `gorm:"column:login_date" json:"login_date,omitempty" redis:"login_date"`
 }
 
 // Hash user password with bcrypt
